af01ce718f9f57983bdff3d523e057d60f0993f9
Nathaniel Biggs <nbiggs@andrew.cmu.edu>
2018-02-16 03:04:23 -0500

    All the JS features have been implemented!

--- crosswords.js
+++ crosswords.js
@@ -11,6 +11,19 @@ const makeSquare = (x,y) => {
   const letterHolder = $("<span>");
   letterHolder.addClass("letter-holder");
   square.append(letterHolder);
+  if (!cellInfo[x]) {
+    cellInfo[x] = {};
+  }
+  cellInfo[x][y] = {
+    number: 0, 
+    filled: false, 
+    letter: ' ', 
+    clues: {'down': 0, 'across': 0},
+    elem: square,
+    highlighted: false,
+    letterElem: letterHolder, 
+    numberElem: numHolder
+  };
   return square;
 };
 
@@ -20,62 +33,100 @@ const makeBoard = (dims) => {
   $("#board-holder").append(board)
   for (let i=0; i < dims[0]; i++) {
     const row = $("<tr>");
-	row.addClass("board-row");
+    row.addClass("board-row");
     for (let j=0; j < dims[1]; j++) {
-	  row.append(makeSquare(i,j));
-	}
-	board.append(row);
+      row.append(makeSquare(i,j));
+    }
+    board.append(row);
   }
 }
 
-const makeClue = (cluesElem) => (c) => {
+const makeClue = (cluesElem, dir) => (c) => {
   const clue = $("<div>");
   clue.addClass("clue");
   clue.text(c[0] + ". " + c[1]);
-  $(clue).attr("clue-num", c[0]);
+  $(clue).attr("num", c[0]);
+  $(clue).attr("dir", dir);
   cluesElem.append(clue);
+  clueInfo[dir][c[0]] = {
+    text: c[1], 
+    cells: [],
+    solved: 0,
+    selected: false,
+    elem: clue
+  };
   $(clue).click(handleClueClick(clue));
 }
 
-const fillCells = (cells) => {
-  cells.forEach((cell) => {
-	getCell(cell).addClass("filled-cell");
+const fillCells = (locs) => {
+  locs.forEach((loc) => {
+    getCell(loc).filled = true;
   });
 }
 
 const addClues = (across, down) => {
   const acrossClues = $("#across-clues");
   const downClues = $("#down-clues");
-  across.forEach(makeClue(acrossClues));
-  down.forEach(makeClue(downClues));
+  across.forEach(makeClue(acrossClues, 'across'));
+  down.forEach(makeClue(downClues, 'down'));
 }
 
 const addNumbers = (numbers) => {
   numbers.forEach((number) => {
     const cell = getCell([number[0],number[1]]);
-	$(cell).attr("num", number[2]);
-	cell.children(".num-holder").text(number[2]);
-	cell.click(() => {
-	  selectClue($(`[clue-num='${number[2]}']`));
-	})
+    cell.number = number[2];
   });
 }
 
+const associateCells = () => {
+  let curAcross;
+  let downDict = {};
+  for (let i = 0; i < dims[0]; i++) {
+    for (let j = 0; j < dims[1]; j++) {
+      const cell = getCell([i,j]);
+      if (cell.filled) {
+        downDict[j] = null;
+        curAcross = null;
+        continue;
+      }
+      if (!curAcross) {
+        curAcross = cell.number;
+      } 
+      cell.clues['across'] = curAcross;
+      clueInfo['across'][curAcross].cells.push([i,j]);
+      if (!downDict[j]) {
+        downDict[j] = cell.number;
+      }
+      cell.clues['down'] = downDict[j];
+      clueInfo['down'][downDict[j]].cells.push([i,j]);
+    }
+    curAcross = null;
+  }
+}
+
+const renderAllCells = () => {
+  for (let i = 0; i < dims[0]; i++) {
+    for (let j = 0; j < dims[1]; j++) {
+      renderCell([i,j]);
+    }
+  }
+}
+
 const setup = () => {
-  dims = [15,15]
-  filled = [];
-  makeBoard(dims);
-  $(document).keydown(handleKeypress(dims, filled));
+  dims = [15,15];
   const socket = new WebSocket('ws://localhost:5678');
   socket.onmessage = (event) => {
-  const data = JSON.parse(event.data);
-  const acrossClues = data['across'];
-  const downClues = data['down'];
-  const cellNumbers = data['nums'];
-  addNumbers(cellNumbers);
-  addClues(acrossClues, downClues);
-  filled = data['filled'];
-  fillCells(data['filled']);
+    const data = JSON.parse(event.data);
+    makeBoard(dims);
+    $(document).keydown(handleKeypress(dims));
+    const acrossClues = data['across'];
+    const downClues = data['down'];
+    const cellNumbers = data['nums'];
+    addClues(acrossClues, downClues);
+    addNumbers(data['nums']);
+    fillCells(data['filled']);
+    associateCells();
+    renderAllCells();
   };
 }
 
@@ -83,60 +134,77 @@ const setup = () => {
 
 // BEGIN GETTERS
 
-const getCell = (cell) => {
-  return $(`td[loc='${cell[0]},${cell[1]}']`);
+const getCell = (loc) => {
+  if (loc[0] < 0 || loc[0] >= dims[0] || loc[1] < 0 || loc[1] >= dims[1]) {
+    return null;
+  }
+  return cellInfo[loc[0]][loc[1]];
 }
 
-const getClue = (clue) => {
-  return $(`[clue-num='${clue}']`)
+const getClue = (clueId) => {
+  return clueInfo[clueId[0]][clueId[1]];
 }
 
 const getLocForCell = (cellElement) => {
   const loc = cellElement.attr("loc");
-  if (!loc) {
-    return null;
-  }
   const xy = loc.split(",");
   return xy.map((x) => parseInt(x));
 }
 
-const getCellForClue = (clue) => {
-  const cellElement = $(`[num='${clue}']`);
-  return getLocForCell(cellElement);
+const getClueDirForClue = (clueElement) => {
+  return clueElement.attr("dir");
 }
 
 const getClueNumForClue = (clueElement) => {
-  return clueElement.attr("clue-num");
+  return clueElement.attr("num");
 }
 
 // END GETTERS
 
 // BEGIN UPDATERS
-
-const updateSelectedCell = (newSelected, opt_skipUpdateClue) => {
-  const invalidCode = validateCell(newSelected);
-  if (invalidCode) {
-	return invalidCode;
-  }
-  getCell(selectedCell).removeClass("selected-cell");
-  selectedCell = newSelected;
-  const cell = getCell(selectedCell)
-  cell.addClass("selected-cell");
-  const num = cell.attr("num");
-  if (!opt_skipUpdateClue) {
-	updateSelectedClue(num, true);
-  }
-  return true;
+const rotateSelected = () => {
+  const cell = getCell(selectedCell);
+  const newDir = selectedClue[0] === 'down' ? 'across' : 'down';
+  updateSelectedClue([newDir, cell.clues[newDir]]);
 }
 
-const updateSelectedClue = (newSelected, opt_skipUpdateCell) => {
-  getClue(selectedClue).removeClass("selected");
-  selectedClue = newSelected;
-  getClue(selectedClue).addClass("selected");
-  const cell = getCellForClue(newSelected);
-  if (!opt_skipUpdateCell && !!cell) {
-	updateSelectedCell(cell, true);
+const updateSelectedCell = (newSelected) => {
+  if (newSelected[0] === selectedCell[0] && newSelected[1] === selectedCell[1]) {
+    rotateSelected();
+    return; 
   }
+  const oldCell = getCell(selectedCell);
+  if (oldCell) {
+    oldCell.selected = false;
+    renderCell(selectedCell);
+  }
+  const newCell = getCell(newSelected);
+  newCell.selected = true;
+  selectedCell = newSelected;
+  renderCell(selectedCell);
+  updateSelectedClue([selectedClue[0], newCell.clues[selectedClue[0]]]);
+}
+
+const updateSelectedClue = (newSelected) => {
+  const oldClue = getClue(selectedClue);
+  if (oldClue) {
+    oldClue.selected = false;
+    renderClue(selectedClue);
+    oldClue.cells.forEach((loc) => {
+      const cell = getCell(loc);
+      cell.highlighted = false;
+      renderCell(loc);
+    });
+  }
+  const newClue = getClue(newSelected);
+  newClue.selected = true;
+  selectedClue = newSelected;
+  renderClue(selectedClue);
+  newClue.cells.forEach((loc) => {
+    const cell = getCell(loc);
+    cell.highlighted = true;
+    renderCell(loc);
+  });
 }
 
 const selectCell = (cellElement) => {
@@ -144,21 +212,79 @@ const selectCell = (cellElement) => {
 }
 
 const selectClue = (clueElement) => {
-  updateSelectedClue(getClueNumForClue(clueElement));
+  updateSelectedClue(getClueDirForClue(clueElement), getClueNumForClue(clueElement));
 }
-
 const putCharInSelected = (c) => {
   const cell = getCell(selectedCell); 
-  cell.children(".letter-holder")
-    .text(String.fromCharCode(c));
-  if (c !== 32) {
-    cell.addClass("has-letter");
-  } else {
-	cell.removeClass("has-letter");
+  const oldLetter = cell.letter;
+  const newLetter = String.fromCharCode(c);
+  cell.letter = newLetter;
+  let delta = 0;
+  if (oldLetter === ' ' && newLetter !== ' ') {
+    delta = 1;
   }
+  if (oldLetter !== ' ' && newLetter === ' ') {
+    delta = -1;
+  }
+  const downClue = getClue(['down', cell.clues['down']]);
+  downClue.solved += delta;
+  const acrossClue = getClue(['across', cell.clues['across']]);
+  acrossClue.solved += delta;
+  renderCell(selectedCell);
+  renderClue(['down', cell.clues['down']]);
+  renderClue(['across', cell.clues['across']]);
 }
 
 // END UPDATERS
+
+// BEGIN RENDERERS
+
+const renderCell = (loc) => {
+  const info = cellInfo[loc[0]][loc[1]];
+  if (info.number) {
+    info.numberElem.text(info.number);
+  } else {
+    info.numberElem.text('');
+  }
+  if (info.filled) {
+    info.elem.addClass('filled-cell');
+  } else {
+    info.elem.removeClass('filled-cell');
+  }
+  if (info.letter !== ' ') {
+    info.elem.addClass('has-letter');
+  } else {
+    info.elem.removeClass('has-letter');
+  }
+  info.letterElem.text(info.letter);
+  if (info.highlighted) {
+    info.elem.addClass('highlighted-cell');
+  } else {
+    info.elem.removeClass('highlighted-cell');
+  }
+  if (info.selected) {
+    info.elem.addClass('selected-cell');
+  } else {
+    info.elem.removeClass('selected-cell');
+  }
+}
+
+const renderClue = (c) => {
+  const clue = clueInfo[c[0]][c[1]];
+  clue.elem.text(`${c[1]}. ${clue.text}`);
+  if (clue.selected) {
+    clue.elem.addClass('selected-clue');
+  } else {
+    clue.elem.removeClass('selected-clue');
+  }
+  if (clue.solved === clue.cells.length) {
+    clue.elem.addClass('clue-solved');
+  } else {
+    clue.elem.removeClass('clue-solved');
+  }
+}
+
+// END RENDERERS
 
 // BEGIN HANDLERS
 
@@ -166,44 +292,67 @@ const handleCellClick = (event) => {
   selectCell($(event.currentTarget));
 }
 
-const handleKeypress = (dims, filled) => (event) => {
+const handleKeypress = (dims) => (event) => {
+  const moveSelected = (deltaX, deltaY) => { 
+    const curX = selectedCell[0];
+    const curY = selectedCell[1];
+    let newX = curX + deltaX;
+    let newY = curY + deltaY;
+    let invalidCode;
+    while (invalidCode = validateCell([newX, newY])) {
+      if (invalidCode === 2) {
+	if (deltaX === 1) {
+          newX = -1;
+        } else if (deltaX === -1) {
+          newX = dims[0];
+        } else if (deltaY === 1) {
+          newY = -1;
+        } else if (deltaY === -1) {
+          newY = dims[1];
+        }
+      }
+      newX += deltaX;
+      newY += deltaY;
+    }
+    updateSelectedCell([newX, newY]);
+  }
+  
   if (event.which > 36 && event.which < 41) {
-	let deltaX = 0;
-	let deltaY = 0;
-	switch(event.which) {
-	  case 37:
-		deltaY = -1;
-		break;
-	  case 38:
-		deltaX = -1;
-		break;
-      case 39:
-		deltaY = 1;
-		break;
-      case 40:
-		deltaX = 1;
-		break;		
-	}
-	const curX = selectedCell[0];
-	const curY = selectedCell[1];
-	let newX = curX + deltaX;
-	let newY = curY + deltaY;
-	let invalidCode;
-	while (invalidCode = updateSelectedCell([newX, newY]) === 1) {
-	  newX += deltaX;
-	  newY += deltaY;
-	}
-	if (invalidCode === 2) {
-	  updateSelectedCell([curX, curY]);
-	}
+    let deltaX = 0;
+    let deltaY = 0;
+    switch(event.which) {
+    case 37:
+      deltaY = -1;
+      break;
+    case 38:
+      deltaX = -1;
+      break;
+    case 39:
+      deltaY = 1;
+      break;
+    case 40:
+      deltaX = 1;
+      break;		
+    }    
+    moveSelected(deltaX, deltaY);
   } else if (event.which > 64 && event.which < 91) {
-	putCharInSelected(event.which);
+    putCharInSelected(event.which);
+    let deltaX = 0;
+    let deltaY = 0;
+    if (selectedClue[0] === 'down') {
+      deltaX = 1;
+    } else {
+      deltaY = 1;
+    }
+    moveSelected(deltaX, deltaY);
   } else if (event.which === 8 || event.which === 46) {
-	putCharInSelected(32)
+    putCharInSelected(32);
+  } else if (event.which === 13) {
+    rotateSelected();
   }
 }
 
-const handleClueClick = (clue) => () => {
+const handleClueClick = (clue, dir, num) => () => {
   selectClue(clue);
 }
 
@@ -211,23 +360,22 @@ const handleClueClick = (clue) => () => {
 
 // BEGIN HELPERS
 
-const validateCell = (cell) => {
-  const cellsEq = (cell1) => (cell2) => 
-    cell1[0] === cell2[0] && cell1[1] === cell2[1];
-  if(filled.some(cellsEq([cell[0], cell[1]]))) {
+const validateCell = (loc) => {
+  const cell = getCell(loc);
+  if (cell === null) {
+    return 2;
+  }
+  if (cell.filled) {
     return 1;
   };
-  if (cell[0] >= dims[0] || cell[0] < 0 || 
-	  cell[1] >= dims[1] || cell[1] < 0) {
-	return 2;	  
-  }
   return 0;
 }
 
 // END HELPERS
 
 let selectedCell = [-1,-1];
-let selectedClue = -1;
-let filled, dims;
+let selectedClue = ['across',-1];
+const cellInfo = {};
+const clueInfo = {'across': {}, 'down': {}};
 
 $(document).ready(setup);
