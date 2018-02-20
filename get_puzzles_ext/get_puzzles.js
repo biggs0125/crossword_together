981cae94f7302689b9124032ef091b097b1595f1
Nathaniel Biggs <nbiggs@andrew.cmu.edu>
2018-02-20 06:00:12 -0500

    Added get puzzles extension

--- get_puzzles_ext/get_puzzles.js
+++ get_puzzles_ext/get_puzzles.js
@@ -1,4 +1,5 @@
 doThing = () => {
+  const waittime = 1000;
   const elements = document.getElementsByClassName("ArchiveViewer-calendarItem--1AhvT");
   const elements1 = [];
   for (i in elements) {
@@ -14,32 +15,42 @@ doThing = () => {
     if (e.length === 0) {
       return;
     }
-    const e2 = e[0].getElementsByTagName("a"); 
+    const e2 = e[0].getElementsByTagName("a");
     for (i in e2) {
       x[i] = e2[i];
     }
     return x[1];
   });
-  let x = 0;
+
+  let x = 1;
+
   elements3.forEach(function(e) {
     if (e) {
       setTimeout(() => {
         e.click();
-      }, x * 200);
+      }, 2000 + (x * waittime));
       x++;
     }
-  })
+  });
   setTimeout(() => {
-    const pathname = window.location.pathname;
+    let pathname = window.location.pathname;
     const length = pathname.length;
     const month = pathname.substring(length-2);
     let newpathname;
     if (month === "01") {
       const year = parseInt(pathname.substring(length-7, length-3));
-      newpathname = pathname.replace(year, year-1).replace("01", "12");
+      pathname = pathname.replace(year, year-1);
+      newpathname = pathname.substring(0, length-2) + "12";
     } else {
       const i = parseInt(month);
-      newpathname = pathname.substring(0, length-2) + month.replace(i, i-1); 
+      let newI = month.replace(i, i-1);
+      if (newI.length < 2) {
+        newI = "0" + newI;
+      }
+      newpathname = pathname.substring(0, length-2) + newI;
     }
     window.location.pathname = newpathname;
-  }, x * 200);}
+  }, (x * waittime) + 4000);
+}
+
+window.onload = doThing;
