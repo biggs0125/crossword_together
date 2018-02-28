c7085570ce199980826e422e29e4ddc954d70040
Nathaniel Biggs <biggs0125@gmail.com>
2018-02-28 08:03:35 +0000

    updated ip

--- crosswords.js
+++ crosswords.js
@@ -132,7 +132,7 @@ const setup = () => {
   });
   $('#setup-holder').show();
   $('#game-holder').hide();
-  socket = new WebSocket('ws://74.66.131.19:5678');
+  socket = new WebSocket('ws://174.138.51.66:5678');
   socket.onmessage = (event) => {
     const data = JSON.parse(event.data);
     if (data.error) {
